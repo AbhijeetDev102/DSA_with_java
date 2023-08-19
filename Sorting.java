80d329d556f14b6a1c4754dded32031e0ed2441d
Abhijeet Das <mns102720@gmail.com>
2023-08-19 23:07:01 +0530

    insertion sort

--- Sorting.java
+++ Sorting.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr={64,4,2,8,9,1,10};
-        selectionS(arr);
+        int[] arr1={3,2,1};
+        cyclicS(arr1);
     }
 
     static void bubbleS(int[] arr){
@@ -43,6 +44,33 @@ public class Sorting {
         }
         System.out.println(Arrays.toString(arr));
 
+    }
+   
+
+    static void insertionS(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j>0; j--) {
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclicS(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int current= arr[i];
+            int temp=arr[arr[i]-1];
+            arr[arr[i]-1]=current;
+            arr[i]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
     
 }
